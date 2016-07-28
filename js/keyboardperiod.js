f8ff612a969630d6dda8d57683930129a0f4c120
Westixy <westixy@gmail.com>
2016-07-29 00:08:11 +0200

    0.0.0a1 - actions, blockage, keymap
    + gestion des action
    + gestion des touches
    + gestions des actions multiples
    + gestion du keycode pour la definition de touche (dispo uniquement sur
    FF et chrome
    + ajout du keymap fr_ch.p1

--- js/keyboardperiod.js
+++ js/keyboardperiod.js
@@ -1,13 +1,26 @@
 /**
  * [key Gestion d'une touche]
- * @param  {int} code [the reference of the key in real]
- * @param  {var} elem [the value of the key]
- * @return {key}      [the object]
+ * @param  {string} code [the reference of the key in real]
+ * @param  {int} which [the reference of the key]
+ * @param  {string} elem [the value of the key]
+ * @return {key} [the object]
  */
-function Key(code,elem){
-    this.code=null||code;
-    this.elem=null||elem;
-    this.action=null;
+function Key(code,which,elem){
+    this.code=code||null;
+    this.which=which||null;
+    this.elem=elem||null;
+    this.actionDown=new KeyActionArray();
+    this.actionPress=new KeyActionArray();
+    this.actionUp=new KeyActionArray();
+    this.toStr=function(){
+        return "[ "+this.code+" => "+this.elem+" ("+this.which+") ]";
+    };
+    this.itsMeC=function(code){
+        return (this.code==code) ;
+    }
+    this.itsMeW=function(which){
+        return (this.which==which) ;
+    }
 }
 
 /**
@@ -15,19 +28,121 @@ function Key(code,elem){
  * @return {keyArray} [the object]
  */
 function KeyArray(){
+    var that=this;
     this.keys=[];
-    this.addKey=function(code,elem){
-        this.keys[this.keys.length]=new Key(code,elem);
+    this.addKey=function(code,which,elem){
+        this.keys.push(new Key(code,which,elem));
     }
-    this.length=this.keys.length;
+    this.length=function(){return this.keys.length;}
+    this.get=function(id){
+        return this.keys[id];
+    }
+    this.getC=function(code){
+        for(var i=0;i<this.length();i++){
+            if(this.keys[i].itsMeC(code)) return this.keys[i];
+        }
+    }
+    this.getW=function(which){
+        for(var i=0;i<this.length();i++){
+            if(this.keys[i].itsMeW(which)) return this.keys[i];
+        }
+    }
+    this.execute=function(ev){
+        var k = (typeof ev.originalEvent.code == "undefined") ? that.getW(ev.originalEvent.which) : that.getC(ev.originalEvent.code);
+
+        if(typeof k == "undefined") return;
+
+        if(ev.type=="keydown"){
+            k.actionDown.execute();
+        return;}
+        if(ev.type=="keypress"){
+            k.actionPress.execute();
+        return;}
+        if(ev.type=="keyup"){
+            k.actionUp.execute();
+        return;}
+    }
+    // Hydratation du clavier depuis un array
+    // Exemple d'array :
+    // [["KeyA",81,"q"],["KeyB",66,"b"],["KeyC",67,"c"]]
+    this.hydrateFromArray=function(array){
+        array.forEach(function(item,index){
+            that.addKey(item[0],item[1],item[2]);
+        });
+    }
+    this.displaySelf=function(){
+        this.keys.forEach(function(item,index){
+            item.actionDown.addAction(function(){console.log(item.toStr());});
+        });
+    }
 }
-
-
-
+/**
+ * [KeyAction action utilisable]
+ * @param {action} [L'action a executer]
+ * @param {condition} [la condition à l'execution de l'action]
+ * @return {KeyAction} [the object]
+ */
+function KeyAction(action,condition){
+    this.action=action||function(){console.log("default key action");}
+    this.condition=condition||true;
+    this.execute=function(){
+        var cond = (typeof this.condition == "function") ? this.condition():this.condition;
+        if(cond){
+            this.action();
+        }
+    }
+}
+/**
+ * [KeyActionArray Gestionnaire d'actions]
+ * @return {KeyActionArray} [the object]
+ */
+function KeyActionArray(){
+    var that = this;
+    this.actions=[];
+    this.addAction=function(action,condition){
+        this.actions.push(new KeyAction(action,condition))
+    }
+    this.length=function(){return this.actions.length;}
+    this.get=function(id){
+        return this.actions[id];
+    }
+    this.executeOne=function(item,index){
+        item.execute();
+    }
+    this.execute=function(){
+        this.actions.forEach(this.executeOne);
+    }
+}
 
 /**
  * [KeyboardPeriod OBJET PRINCIPAL DE GESTION DE CLAVIER]
  */
 function KeyboardPeriod(){
+    var that = this;
+
     this.keya=new KeyArray();
+    this.elemEvent=document;
+    //this.keyme="[";
+
+    this.enableEvents=function(){
+        $(this.elemEvent).on("keypress",this.keya.execute);
+        $(this.elemEvent).on("keydown",this.keya.execute);
+        $(this.elemEvent).on("keyup",this.keya.execute);
+    }
+    this.setKeymap=function(array){
+        this.keya.keys=[];
+        this.keya.hydrateFromArray(array);
+    }
+
+    this.fixFackingKeys=function(){
+        $(document).on("keydown",disableF5);
+        $(document).on("keydown",disableBCKSP)
+    }
+
+    this.enableEvents();
+    this.fixFackingKeys();
 }
+
+// fixFackingKeys suite
+function disableF5(e) { if ((e.which || e.keyCode) == 116) e.preventDefault(); };
+function disableBCKSP(e) { if ((e.which || e.keyCode) == 8) e.preventDefault(); };
